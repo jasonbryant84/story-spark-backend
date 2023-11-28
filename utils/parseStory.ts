8b25a75805f381a71f3c2bdf2cc3b4c6138af187
Jason Bryant <>
2023-11-28 16:13:10 +0000

    temp hack for parseStory logic to reduce API calls, etc.

--- utils/parseStory.ts
+++ utils/parseStory.ts
@@ -1,3 +1,14 @@
+const pairOffElements = (array: string[]) => {
+    const pairs = [];
+  
+    for (let i = 0; i < array.length - 1; i += 2) {
+      const pair = [array[i], array[i + 1]]
+      pairs.push(pair.join(''))
+    }
+  
+    return pairs;
+}
+
 export const parseStory = (initialStory: string) => {
     const splitStory = initialStory.split('\n')
 
@@ -11,10 +22,12 @@ export const parseStory = (initialStory: string) => {
     const content = splitStory.slice(2).join('\n')
 
     // skipping the title and the first \n
-    const contentArray = splitStory.slice(2).filter(e => e.length > 0) // excluding ''
+    let contentArray = splitStory.slice(2).filter(e => e.length > 0) // excluding ''
+    contentArray = pairOffElements(contentArray) // only needed to reduce API requests for now
 
-    // Use Each paragraph as an image prompt
-    const imagePrompts = splitStory.slice(2).filter(e => e.length > 0) // excluding ''
+    // TODO: redundant remove this - Use Each paragraph as an image prompt
+    let imagePrompts = splitStory.slice(2).filter(e => e.length > 0) // excluding ''
+    imagePrompts = pairOffElements(imagePrompts) // only needed to reduce API requests for now
 
     return { title, titleHyphenated, content, contentArray, imagePrompts }
 }
